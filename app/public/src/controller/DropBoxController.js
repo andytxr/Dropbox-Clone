9f0a9669338cd05661adc1b37bcca12f63409fef
decosawa <andreluiiz.gst@gmail.com>
2022-04-13 18:54:47 -0300

    Funcionalidade de tempo restante do upload

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -6,6 +6,8 @@ class DropBoxController {
         this.inputFilesEl = document.getElementById('files');
         this.snackModalEl = document.getElementById('react-snackbar-root');
         this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg');
+        this.nameFileEl = this.snackModalEl.querySelector('.filename');
+        this.timeLeftEl = this.snackModalEl.querySelector('.timeleft');
 
         this.initEvents();
 
@@ -71,6 +73,7 @@ class DropBoxController {
 
                 let formData= new FormData();
                 formData.append('input-file', file)
+                this.startUpTime = Date.now();
 
                 ajax.send(formData);
 
@@ -86,10 +89,38 @@ class DropBoxController {
 
         let loaded = event.loaded;
         let total = event.total;
-
         let porcent = parseInt((loaded/total)*100);
 
+        let timespent = Date.now() - this.startUpTime;
+        let timeleft = ((100 - porcent)*timespent)/porcent;
+        
+
         this.progressBarEl.style.width=`${porcent}%`;
+        this.nameFileEl.innerHTML=file.name;
+        this.timeLeftEl.innerHTML= this.formatTime(timeleft);
+
+        console.log(timespent, timeleft, porcent);
+
+    }
+
+    formatTime(duration){
+
+        let sec = parseInt((duration/1000)%60);
+        let min = parseInt(duration*(1000*60)%60);
+        let hours = parseInt(duration*(1000*60*60)%24);
+
+        if(hours > 0){
+            return `${hours} horas, ${min} minutos e ${sec} segundos.`
+        } else if(min>0){
+            return `${min} minutos e ${sec} segundos.`
+        }
+        else if(sec>0){
+            return `${sec} segundos.`
+        }
+
+        return '';
+
+
 
     }
 }
