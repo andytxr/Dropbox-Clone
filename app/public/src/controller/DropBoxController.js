5a32c6ad4419d5231ed0d9cd527c1ccd1fb7fab2
decosawa <andreluiiz.gst@gmail.com>
2022-04-13 17:38:54 -0300

    Iniciando classe DropBoxController

---
+++ app/public/src/controller/DropBoxController.js
@@ -0,0 +1,74 @@
+class DropBoxController {
+
+    constructor(){
+
+        this.btnSendFileEl = document.getElementById('btn-send-file');
+        this.inputFilesEl = document.getElementById('files');
+        this.snackModalEl = document.getElementById('react-snackbar-root');
+
+        this.initEvents();
+
+    }
+
+    initEvents(){
+
+        this.btnSendFileEl.addEventListener('click', event =>{
+
+            this.inputFilesEl.click();
+
+        })
+
+        this.inputFilesEl.addEventListener('change', event =>{
+
+            console.log(event.target.files);
+            this.uploadTasks(files);
+
+            this.snackModalEl.style.display='block';
+
+        })
+
+    }
+
+    uploadTasks(files){
+
+        let promises = [];
+
+        [...files].forEach(file=>{
+
+            promises.push(new Promise((resolve, reject)=>{
+
+                let ajax = new XMLHttpRequest();
+                ajax.open('POST', '/upload');
+                ajax.onload=event=>{
+
+                    try{
+
+                        resolve(JSON.parse(ajax.responseText));
+
+                    }catch(e){
+
+                        reject(e);
+
+                    }
+
+                }
+
+                ajax.onerror=event=>{
+
+                    reject(event);
+
+                }
+
+                let formData= new FormData();
+                formData.append('input-file', file)
+
+                ajax.send(formData);
+
+            }));
+
+        })
+
+        return Promise.all(promises);
+
+    }
+}
